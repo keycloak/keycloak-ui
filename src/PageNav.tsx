a54142e56997fd43fb8e83027ec7887ec315af36
Stan Silvert <ssilvert@redhat.com>
2020-09-08 10:43:56 -0400

    Flesh out left navigation

--- src/PageNav.tsx
+++ src/PageNav.tsx
@@ -1,27 +1,62 @@
-import React from "react";
-import { Nav, NavItem, NavList, PageSidebar } from "@patternfly/react-core";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Nav, NavItem, NavGroup, PageSidebar } from "@patternfly/react-core";
 import { RealmSelector } from "./components/realm-selector/RealmSelector";
 
 export const PageNav: React.FunctionComponent = () => {
+  const history = useHistory();
+
+  let initialItem = history.location.pathname;
+  if (initialItem === "/") initialItem = "/client-list";
+
+  const [activeItem, setActiveItem] = useState(initialItem);
+
+  type SelectedItem = {
+    groupId: number | string;
+    itemId: number | string;
+    to: string;
+    event: React.FormEvent<HTMLInputElement>;
+  };
+
+  const onSelect = (item: SelectedItem) => {
+    setActiveItem(item.to);
+    history.push(item.to);
+    item.event.preventDefault();
+  };
+
+  const makeNavItem = (title: string, path: string) => {
+    return (
+      <NavItem
+        id={"nav-item-" + path}
+        to={"/" + path}
+        isActive={activeItem === "/" + path}
+      >
+        {title}
+      </NavItem>
+    );
+  };
+
   return (
     <PageSidebar
       nav={
-        <Nav>
-          <NavList>
-            <RealmSelector />
-            <NavItem id="default-link1" to="/default-link1" itemId={0}>
-              Link 1
-            </NavItem>
-            <NavItem id="default-link2" to="/default-link2" itemId={1} isActive>
-              Current link
-            </NavItem>
-            <NavItem id="default-link3" to="/default-link3" itemId={2}>
-              Link 3
-            </NavItem>
-            <NavItem id="default-link4" to="/default-link4" itemId={3}>
-              Link 4
-            </NavItem>
-          </NavList>
+        <Nav onSelect={onSelect}>
+          <RealmSelector />
+          <NavGroup title="Manage">
+            {makeNavItem("Clients", "clients")}
+            {makeNavItem("Client Scopes", "client-scopes")}
+            {makeNavItem("Realm Roles", "realm-roles")}
+            {makeNavItem("Users", "users")}
+            {makeNavItem("Groups", "groups")}
+            {makeNavItem("Sessions", "sessions")}
+            {makeNavItem("Events", "events")}
+          </NavGroup>
+
+          <NavGroup title="Configure">
+            {makeNavItem("Realm settings", "realm-settings")}
+            {makeNavItem("Authentication", "authentication")}
+            {makeNavItem("Identity providers", "identity-providers")}
+            {makeNavItem("User federation", "user-federation")}
+          </NavGroup>
         </Nav>
       }
     />
