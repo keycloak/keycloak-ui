03fb839941a9acbecd7ab3ef48605c2f164258ba
jenny-s51 <jshandel12@gmail.com>
2020-09-14 16:14:39 -0400

    add storybook demo and role actions kebab

---
+++ src/page/RealmRolesPage.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  Divider,
+  Page,
+  PageSection,
+  PageSectionVariants,
+  Text,
+  TextContent,
+} from "@patternfly/react-core";
+
+import { DataLoader } from "../components/data-loader/DataLoader";
+import { TableToolbar } from "../components/table-toolbar/TableToolbar";
+import { HttpClientContext } from "../http-service/HttpClientContext";
+import { RoleRepresentation } from "../model/role-model";
+import { RolesList } from "../realm-roles/RoleList";
+// import { NoRealmRolesPage } from "../realm-roles/NoRealmRoles";
+
+export const RealmRolesPage = () => {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const [max, setMax] = useState(10);
+  const [roles, setRoles] = useState([] as RoleRepresentation[]);
+  const [first, setFirst] = useState(0);
+  const httpClient = useContext(HttpClientContext)!;
+
+  const loader = async () => {
+    return await httpClient
+      .doGet("/admin/realms/master/roles")
+      .then((r) => r.data as RoleRepresentation[]);
+  };
+
+  useEffect(() => {
+    loader().then((result) => (setRoles(result) !== undefined ? result : []));
+  }, []);
+
+  return (
+    <DataLoader loader={loader}>
+      {(roles) => (
+        <>
+          <PageSection
+            className="rolesHeader"
+            variant={PageSectionVariants.light}
+          >
+            <TextContent className="rolesDescription">
+              <Text component="h1">Realm roles</Text>
+              <Text component="p">
+                Realm-level roles are a global namespace to define your roles.
+              </Text>
+            </TextContent>
+          </PageSection>
+          <Divider component="li" key={1} />
+          <PageSection className="rolesTable">
+            <TableToolbar
+              count={roles!.length}
+              first={first}
+              max={max}
+              onNextClick={setFirst}
+              onPreviousClick={setFirst}
+              onPerPageSelect={(f, m) => {
+                setFirst(f);
+                setMax(m);
+              }}
+              toolbarItem={
+                <>
+                  <Button onClick={() => history.push("/add-role")}>
+                    {t("Create role")}
+                  </Button>
+                </>
+              }
+            >
+              <RolesList roles={roles} />
+            </TableToolbar>
+          </PageSection>
+        </>
+      )}
+    </DataLoader>
+  );
+};
