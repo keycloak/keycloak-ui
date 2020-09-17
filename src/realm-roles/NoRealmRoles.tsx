319efc2cf922ab315e6725745e87328e841be5b5
jenny-s51 <jshandel12@gmail.com>
2020-09-17 13:29:52 -0400

    localize NoRealmRoles component

--- src/realm-roles/NoRealmRoles.tsx
+++ src/realm-roles/NoRealmRoles.tsx
@@ -11,22 +11,22 @@ import {
 import { useHistory } from "react-router-dom";
 
 import { PlusCircleIcon } from "@patternfly/react-icons";
+import { useTranslation } from "react-i18next";
 
 export const NoRealmRolesPage = () => {
+  const { t } = useTranslation("realm");
   const history = useHistory();
-  const emptyStateText =
-    "There aren't any realm roles in this realm. Create a realm role to get started";
   return (
     <>
       <PageSection>
         <EmptyState variant={EmptyStateVariant.large}>
           <EmptyStateIcon icon={PlusCircleIcon} />
           <Title headingLevel="h4" size="lg">
-            No realm roles
+          {t("noRealmRoles")}
           </Title>
-          <EmptyStateBody>{emptyStateText}.</EmptyStateBody>
+          <EmptyStateBody>{t("emptyStateText")}</EmptyStateBody>
           <Button variant="primary" onClick={() => history.push("/add-role")}>
-            Create Realm
+            {t("createRealm")}
           </Button>
         </EmptyState>
       </PageSection>
