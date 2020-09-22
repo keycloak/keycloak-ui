ff6f1e7c36125d9e9367ff26b7799ab15ac70749
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-09-22 08:01:54 +0200

    fix merge error

--- src/clients/ClientSettings.tsx
+++ src/clients/ClientSettings.tsx
@@ -9,7 +9,9 @@ import {
   DropdownItem,
   Switch,
   TextArea,
+  PageSection,
 } from "@patternfly/react-core";
+import { useForm } from "react-hook-form";
 
 import { ScrollForm } from "../components/scroll-form/ScrollForm";
 import { ClientDescription } from "./ClientDescription";
@@ -23,6 +25,7 @@ type ClientSettingsProps = {
 export const ClientSettings = ({ client: clientInit }: ClientSettingsProps) => {
   const { t } = useTranslation("clients");
   const [client, setClient] = useState({ ...clientInit });
+  const form = useForm();
   const onChange = (
     value: string | boolean,
     event: FormEvent<HTMLInputElement>
@@ -36,94 +39,99 @@ export const ClientSettings = ({ client: clientInit }: ClientSettingsProps) => {
     });
   };
   return (
-    <ScrollForm
-      sections={[
-        t("capabilityConfig"),
-        t("generalSettings"),
-        t("accessSettings"),
-        t("loginSettings"),
-      ]}
-    >
-      <Form isHorizontal>
-        <Step2 client={client} onChange={onChange} />
-      </Form>
-      <Form isHorizontal>
-        <ClientDescription client={client} onChange={() => {}} />
-      </Form>
-      <Form isHorizontal>
-        <FormGroup label={t("rootUrl")} fieldId="kc-root-url">
-          <TextInput
-            type="text"
-            id="kc-root-url"
-            name="rootUrl"
-            value={client.rootUrl}
-            onChange={onChange}
-          />
-        </FormGroup>
-        <FormGroup label={t("validRedirectUri")} fieldId="kc-redirect">
-          <TextInput
-            type="text"
-            id="kc-redirect"
-            name="redirectUris"
-            onChange={onChange}
-          />
-        </FormGroup>
-        <FormGroup label={t("homeURL")} fieldId="kc-home-url">
-          <TextInput
-            type="text"
-            id="kc-home-url"
-            name="baseUrl"
-            value={client.baseUrl}
-            onChange={onChange}
-          />
-        </FormGroup>
-      </Form>
-      <Form isHorizontal>
-        <FormGroup label={t("loginTheme")} fieldId="kc-login-theme">
-          <Dropdown
-            id="kc-login-theme"
-            toggle={
-              <DropdownToggle id="toggle-id" onToggle={() => {}}>
-                {t("loginTheme")}
-              </DropdownToggle>
-            }
-            dropdownItems={[
-              <DropdownItem key="link">Link</DropdownItem>,
-              <DropdownItem key="action" component="button" />,
-            ]}
-          />
-        </FormGroup>
-        <FormGroup label={t("consentRequired")} fieldId="kc-consent">
-          <Switch
-            id="kc-consent"
-            name="consentRequired"
-            label={t("common:on")}
-            labelOff={t("common:off")}
-            isChecked={client.consentRequired}
-            onChange={onChange}
-          />
-        </FormGroup>
-        <FormGroup label={t("displayOnClient")} fieldId="kc-display-on-client">
-          <Switch
-            id="kc-display-on-client"
-            name="alwaysDisplayInConsole"
-            label={t("common:on")}
-            labelOff={t("common:off")}
-            isChecked={client.alwaysDisplayInConsole}
-            onChange={onChange}
-          />
-        </FormGroup>
-        <FormGroup
-          label={t("consentScreenText")}
-          fieldId="kc-consent-screen-text"
-        >
-          <TextArea
-            id="kc-consent-screen-text"
-            name="consentText"
-            //value={client.protocolMappers![0].consentText}
-          />
-        </FormGroup>
-      </Form>
-    </ScrollForm>
+    <PageSection>
+      <ScrollForm
+        sections={[
+          t("capabilityConfig"),
+          t("generalSettings"),
+          t("accessSettings"),
+          t("loginSettings"),
+        ]}
+      >
+        <Form isHorizontal>
+          <Step2 form={form} />
+        </Form>
+        <Form isHorizontal>
+          <ClientDescription register={form.register} />
+        </Form>
+        <Form isHorizontal>
+          <FormGroup label={t("rootUrl")} fieldId="kc-root-url">
+            <TextInput
+              type="text"
+              id="kc-root-url"
+              name="rootUrl"
+              value={client.rootUrl}
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup label={t("validRedirectUri")} fieldId="kc-redirect">
+            <TextInput
+              type="text"
+              id="kc-redirect"
+              name="redirectUris"
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup label={t("homeURL")} fieldId="kc-home-url">
+            <TextInput
+              type="text"
+              id="kc-home-url"
+              name="baseUrl"
+              value={client.baseUrl}
+              onChange={onChange}
+            />
+          </FormGroup>
+        </Form>
+        <Form isHorizontal>
+          <FormGroup label={t("loginTheme")} fieldId="kc-login-theme">
+            <Dropdown
+              id="kc-login-theme"
+              toggle={
+                <DropdownToggle id="toggle-id" onToggle={() => {}}>
+                  {t("loginTheme")}
+                </DropdownToggle>
+              }
+              dropdownItems={[
+                <DropdownItem key="link">Link</DropdownItem>,
+                <DropdownItem key="action" component="button" />,
+              ]}
+            />
+          </FormGroup>
+          <FormGroup label={t("consentRequired")} fieldId="kc-consent">
+            <Switch
+              id="kc-consent"
+              name="consentRequired"
+              label={t("common:on")}
+              labelOff={t("common:off")}
+              isChecked={client.consentRequired}
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup
+            label={t("displayOnClient")}
+            fieldId="kc-display-on-client"
+          >
+            <Switch
+              id="kc-display-on-client"
+              name="alwaysDisplayInConsole"
+              label={t("common:on")}
+              labelOff={t("common:off")}
+              isChecked={client.alwaysDisplayInConsole}
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup
+            label={t("consentScreenText")}
+            fieldId="kc-consent-screen-text"
+          >
+            <TextArea
+              id="kc-consent-screen-text"
+              name="consentText"
+              //value={client.protocolMappers![0].consentText}
+            />
+          </FormGroup>
+        </Form>
+      </ScrollForm>
+    </PageSection>
   );
 };
