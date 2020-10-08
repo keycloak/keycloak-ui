d00dd4fd9dc72d249e3846dd201befdcca12f38c
Sarah Rambacher <srambach@redhat.com>
2020-10-08 15:21:36 -0400

    remove commented out code

--- src/components/help-enabler/HelpItem.tsx
+++ src/components/help-enabler/HelpItem.tsx
@@ -16,12 +16,6 @@ export const HelpItem = ({ helpText, forLabel, forID }: HelpItemProps) => {
   return (
     <>
       {enabled && (
-        // <Tooltip position="right" content={t(`help:${item}`)}>
-        //   <span id={item} data-testid={item}> *** I don't know what data-testid was being used for ***
-        //     <HelpIcon />
-        //   </span>
-        // </Tooltip>
-        // <Popover bodyContent={t(`help:${item}`)}>
         <Popover bodyContent={helpText}>
           <button
             aria-label={t(`helpLabel`, { label: forLabel })}
