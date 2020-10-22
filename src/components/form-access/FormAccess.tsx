e09dbeb258da79abe415fd5e77b2f9c56bb3b0b5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-10-22 16:29:26 +0200

    better render of `<Controller` wrapped components

--- src/components/form-access/FormAccess.tsx
+++ src/components/form-access/FormAccess.tsx
@@ -5,8 +5,7 @@ import React, {
   ReactElement,
 } from "react";
 import { Controller } from "react-hook-form";
-import { useTranslation } from "react-i18next";
-import { Form, FormProps, Switch } from "@patternfly/react-core";
+import { Form, FormProps } from "@patternfly/react-core";
 
 import { useAccess } from "../../context/access/Access";
 import { AccessType } from "../../context/whoami/who-am-i-model";
@@ -27,30 +26,6 @@ export const FormAccess = ({
 }: FormAccessProps) => {
   const { hasAccess } = useAccess();
 
-  const ReadOnly = (props: any) => {
-    const { t } = useTranslation();
-    if (typeof props.value === "boolean") {
-      return (
-        <Switch
-          id={props.name}
-          isDisabled
-          label={t("common:on")}
-          labelOff={t("common:off")}
-          isChecked={props.value}
-          onChange={props.onChange}
-        />
-      );
-    }
-    return (
-      <input
-        type="text"
-        value={props.value}
-        onChange={props.onChange}
-        disabled
-      />
-    );
-  };
-
   const recursiveCloneChildren = (children: ReactElement[], newProps: any) => {
     return Children.map(children, (child: ReactElement) => {
       if (!isValidElement(child)) {
@@ -61,10 +36,14 @@ export const FormAccess = ({
         if (child.type === Controller && newProps.isDisabled) {
           return cloneElement(child, {
             ...(child as ReactElement).props,
-            // eslint-disable-next-line react/display-name
-            render: (props: any) => (
-              <ReadOnly {...props} name={(child as ReactElement).props.name} />
-            ),
+            render: (props: any) => {
+              const element = (child as ReactElement).props.render(props);
+              return cloneElement(element, {
+                value: props.value,
+                onChange: props.onChange,
+                isDisabled: newProps.isDisabled,
+              });
+            },
           });
         }
         newProps.children = recursiveCloneChildren(
