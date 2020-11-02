f002b243dd33b6f8b2515d52359dc5f0ef0bc643
Sarah Rambacher <srambach@redhat.com>
2020-11-02 13:41:30 -0500

    lays out fields for user fed kerberos settings

---
+++ src/user-federation/UserFederationKerberosSettings.tsx
@@ -0,0 +1,35 @@
+import {
+  PageSection,
+  Text,
+  TextContent,
+  TextVariants,
+  Title,
+} from "@patternfly/react-core";
+import { useTranslation } from "react-i18next";
+import React from "react";
+import { ScrollForm } from "../components/scroll-form/ScrollForm";
+import { KerberosSettingsRequired } from "./KerberosSettingsRequired";
+import { KerberosSettingsCache } from "./KerberosSettingsCache";
+
+export const UserFederationKerberosSettings = () => {
+  const { t } = useTranslation("user-federation");
+
+  return (
+    <>
+      <PageSection variant="light">
+        {/* Required settings */}
+        <Title size={"xl"} headingLevel={"h2"} className="pf-u-mb-lg">
+          {t("requiredSettings")}
+        </Title>
+        <KerberosSettingsRequired />
+      </PageSection>
+      <PageSection variant="light" isFilled>
+        {/* Cache settings */}
+        <Title size={"xl"} headingLevel={"h2"} className="pf-u-mb-lg">
+          {t("cacheSettings")}
+        </Title>
+        <KerberosSettingsCache />
+      </PageSection>
+    </>
+  );
+};
