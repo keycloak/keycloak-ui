867ba14124035416aeef4f3a7005b3f9f952c055
Alfredo Boullosa <aboullos@redhat.com>
2021-01-12 16:04:32 +0100

    Modify client_scope test messages

--- tests/cypress/integration/00_set_new_admin_console.spec.js
+++ tests/cypress/integration/00_set_new_admin_console.spec.js
@@ -2,18 +2,18 @@ import LoginPage from '../support/pages/LoginPage.js'
 import WelcomePage from '../support/pages/WelcomePage.js'
 import OldClientPage from '../support/pages/admin_console/manage/clients/OldClientPage.js'
 
-describe('Client Scopes test', function () {
+describe('Set up test', function () {
 
     const loginPage = new LoginPage();
     const welcomePage = new WelcomePage();
     const oldClientPage = new OldClientPage();
   
-    describe('Client Scope creation', function () {
+    describe('Set up test', function () {
       beforeEach(function () {
         cy.visit('http://localhost:8180/auth')
       })
 
-      it('create admin user', function () {
+      it('Create admin user and adds admin console client', function () {
           welcomePage
               .createAdminUser()
               .goToAdminConsole();
