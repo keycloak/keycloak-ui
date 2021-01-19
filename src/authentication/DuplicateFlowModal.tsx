8249a19854ba2756ba5478972e3c8046f91988e5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-01-19 13:05:30 +0100

    initial version of authentication section

---
+++ src/authentication/DuplicateFlowModal.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
+import {
+  AlertVariant,
+  Button,
+  ButtonVariant,
+  Form,
+  FormGroup,
+  Modal,
+  ModalVariant,
+  TextInput,
+  ValidatedOptions,
+} from "@patternfly/react-core";
+
+import { useAdminClient } from "../context/auth/AdminClient";
+import { useAlerts } from "../components/alert/Alerts";
+
+type DuplicateFlowModalProps = {
+  name: string;
+  description: string;
+  toggleDialog: () => void;
+};
+
+export const DuplicateFlowModal = ({
+  name,
+  description,
+  toggleDialog,
+}: DuplicateFlowModalProps) => {
+  const { t } = useTranslation("authentication");
+  const { register, errors, setValue, trigger, getValues } = useForm();
+  const adminClient = useAdminClient();
+  const { addAlert } = useAlerts();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setValue("description", description);
+      setValue("name", t("copyOf", { name }));
+    }, 10);
+    return () => clearTimeout(timer);
+  }, [name]);
+
+  const save = async () => {
+    await trigger();
+    const form = getValues();
+    try {
+      await adminClient.authenticationManagement.copyFlow({
+        flow: name,
+        newName: form.name,
+      });
+      if (form.description !== description) {
+        const newFlow = (
+          await adminClient.authenticationManagement.getFlows()
+        ).find((flow) => flow.alias === form.name)!;
+
+        newFlow.description = form.description;
+        await adminClient.authenticationManagement.updateFlow(
+          { flowId: newFlow.id! },
+          newFlow
+        );
+      }
+      addAlert(t("copyFlowSuccess"), AlertVariant.success);
+    } catch (error) {
+      addAlert(t("copyFlowError", { error }), AlertVariant.danger);
+    }
+  };
+
+  return (
+    <Modal
+      title={t("duplicateFlow")}
+      isOpen={true}
+      onClose={toggleDialog}
+      variant={ModalVariant.small}
+      actions={[
+        <Button id="modal-confirm" key="confirm" onClick={save}>
+          {t("common:save")}
+        </Button>,
+        <Button
+          id="modal-cancel"
+          key="cancel"
+          variant={ButtonVariant.secondary}
+          onClick={() => {
+            toggleDialog();
+          }}
+        >
+          {t("common:cancel")}
+        </Button>,
+      ]}
+    >
+      <Form isHorizontal>
+        <FormGroup
+          label={t("common:name")}
+          fieldId="kc-name"
+          helperTextInvalid={t("common:required")}
+          validated={
+            errors.name ? ValidatedOptions.error : ValidatedOptions.default
+          }
+          isRequired
+        >
+          <TextInput
+            type="text"
+            id="kc-name"
+            name="name"
+            ref={register({ required: true })}
+            validated={
+              errors.name ? ValidatedOptions.error : ValidatedOptions.default
+            }
+          />
+        </FormGroup>
+        <FormGroup label={t("common:description")} fieldId="kc-description">
+          <TextInput
+            type="text"
+            id="kc-description"
+            name="description"
+            ref={register()}
+          />
+        </FormGroup>
+      </Form>
+    </Modal>
+  );
+};
