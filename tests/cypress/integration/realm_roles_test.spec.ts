60bf1f68a610f989ee617cd9cae33baff2edb7c4
jenny-s51 <jshandel@redhat.com>
2021-02-03 12:06:31 -0500

    cypress tests, bump react-hook-form to remove console warnings

--- tests/cypress/integration/realm_roles_test.spec.ts
+++ tests/cypress/integration/realm_roles_test.spec.ts
@@ -60,7 +60,11 @@ describe("Realm roles test", function () {
       listingPage.itemExist(itemId, false);
     });
 
+<<<<<<< HEAD
     it("Associated roles modal test", function () {
+=======
+    it("Realm role attributes modal test", function () {
+>>>>>>> cypress tests, bump react-hook-form to remove console warnings
       itemId += "_" + (Math.random() + 1).toString(36).substring(7);
 
       // Create
