b6ea38c7950795c9ae4fae9b809d0383c5322cd8
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-02-17 08:03:52 +0100

    fixed merge error

--- src/components/help-enabler/HelpHeader.tsx
+++ src/components/help-enabler/HelpHeader.tsx
@@ -7,6 +7,7 @@ import {
   Split,
   SplitItem,
   Switch,
+  TextContent,
 } from "@patternfly/react-core";
 import { Trans, useTranslation } from "react-i18next";
 import { HelpIcon, ExternalLinkAltIcon } from "@patternfly/react-icons";
