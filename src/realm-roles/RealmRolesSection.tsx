fb60adbdd1bfef5d0ec0f5a34f1737acb05254b0
jenny-s51 <jshandel@redhat.com>
2021-03-11 13:37:25 -0500

    remove chip group filters

--- src/realm-roles/RealmRolesSection.tsx
+++ src/realm-roles/RealmRolesSection.tsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Button, Chip, ChipGroup, PageSection } from "@patternfly/react-core";
+import { PageSection } from "@patternfly/react-core";
 import { ViewHeader } from "../components/view-header/ViewHeader";
 import { asyncStateFetch, useAdminClient } from "../context/auth/AdminClient";
 import { RolesList } from "./RolesList";
 import { useErrorHandler } from "react-error-boundary";
-import { useTranslation } from "react-i18next";
 
 export const RealmRolesSection = () => {
   const adminClient = useAdminClient();
   const [listRoles, setListRoles] = useState(false);
   const [searchFilters, setSearchFilters] = useState([] as string[]);
   const handleError = useErrorHandler();
-  const { t } = useTranslation("roles");
 
   useEffect(() => {
     return asyncStateFetch(
@@ -56,34 +54,11 @@ export const RealmRolesSection = () => {
     return await adminClient.roles.find(params);
   };
 
-  const handleRemoveItem = (filterName: string) => {
-    setSearchFilters(searchFilters!.filter((item) => item !== filterName));
-  };
-
   return (
     <>
       <ViewHeader titleKey="roles:title" subKey="roles:roleExplain" />
       <PageSection variant="light" padding={{ default: "noPadding" }}>
         <RolesList
-          filterChips={
-            <>
-              <ChipGroup categoryName={t("roles:roleName")}>
-                {searchFilters!.map((currentChip) => (
-                  <Chip
-                    key={currentChip}
-                    onClick={() => handleRemoveItem(currentChip)}
-                  >
-                    {currentChip}
-                  </Chip>
-                ))}
-              </ChipGroup>
-              {searchFilters!.length > 0 && (
-                <Button variant="link" onClick={() => setSearchFilters([])}>
-                  {t("roles:clearAllFilters")}
-                </Button>
-              )}
-            </>
-          }
           searchFilters={searchFilters}
           loader={loader}
         />
