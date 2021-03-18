4ef99e5c1cf226b01190eb90815b385744bb6881
jenny-s51 <jshandel@redhat.com>
2021-03-18 09:19:22 -0400

    lint

--- src/user/UserGroups.tsx
+++ src/user/UserGroups.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useHistory, useParams, useRouteMatch } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import {
   AlertVariant,
