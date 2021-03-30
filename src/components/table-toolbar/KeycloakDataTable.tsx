81db0a60a223d6a87acb45e9df87c74f9f4cb01c
jenny-s51 <jshandel@redhat.com>
2021-03-30 11:11:32 -0400

    remove slice

--- src/components/table-toolbar/KeycloakDataTable.tsx
+++ src/components/table-toolbar/KeycloakDataTable.tsx
@@ -184,7 +184,7 @@ export function KeycloakDataTable<T>({
         return convertToColumns(data);
       },
       (result) => {
-        setRows(result.slice(first, first + max));
+        setRows(result);
         setFilteredData(result);
         setLoading(false);
       },
