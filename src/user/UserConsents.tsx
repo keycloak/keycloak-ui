bc115d7c16464fa6e9fdcadb1ecdebb7b856a231
jenny-s51 <jshandel@redhat.com>
2021-04-13 11:14:17 -0400

    address PR feedback from Stan and list consents in data table

--- src/user/UserConsents.tsx
+++ src/user/UserConsents.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { Button, PageSection } from "@patternfly/react-core";
+import { PageSection } from "@patternfly/react-core";
 import { ListEmptyState } from "../components/list-empty-state/ListEmptyState";
 import { KeycloakDataTable } from "../components/table-toolbar/KeycloakDataTable";
 import { emptyFormatter } from "../util";
@@ -10,6 +10,7 @@ import { cellWidth } from "@patternfly/react-table";
 import _ from "lodash";
 import UserConsentRepresentation from "keycloak-admin/lib/defs/userConsentRepresentation";
 import { CubesIcon } from "@patternfly/react-icons";
+import moment from "moment";
 
 export const UserConsents = () => {
   const { t } = useTranslation("roles");
@@ -26,6 +27,22 @@ export const UserConsents = () => {
     return alphabetize(consents);
   };
 
+  const clientScopesRenderer = ({
+    grantedClientScopes,
+  }: UserConsentRepresentation) => {
+    return <>{grantedClientScopes!.join(", ")}</>;
+  };
+
+  const createdRenderer = ({ createDate }: UserConsentRepresentation) => {
+    return <>{moment(createDate).format("MM/DD/YY hh:MM A")}</>;
+  };
+
+  const lastUpdatedRenderer = ({
+    lastUpdatedDate,
+  }: UserConsentRepresentation) => {
+    return <>{moment(lastUpdatedDate).format("MM/DD/YY hh:MM A")}</>;
+  };
+
   return (
     <>
       <PageSection variant="light">
@@ -34,7 +51,7 @@ export const UserConsents = () => {
           ariaLabelKey="roles:roleList"
           columns={[
             {
-              name: "client",
+              name: "clientId",
               displayKey: "clients:Client",
               cellFormatters: [emptyFormatter()],
               transforms: [cellWidth(20)],
@@ -43,19 +60,21 @@ export const UserConsents = () => {
               name: "grantedClientScopes",
               displayKey: "client-scopes:grantedClientScopes",
               cellFormatters: [emptyFormatter()],
+              cellRenderer: clientScopesRenderer,
               transforms: [cellWidth(30)],
             },
-
             {
-              name: "createDate",
+              name: "createdDate",
               displayKey: "clients:created",
               cellFormatters: [emptyFormatter()],
+              cellRenderer: createdRenderer,
               transforms: [cellWidth(20)],
             },
             {
               name: "lastUpdatedDate",
               displayKey: "clients:lastUpdated",
               cellFormatters: [emptyFormatter()],
+              cellRenderer: lastUpdatedRenderer,
               transforms: [cellWidth(20)],
             },
           ]}
@@ -64,18 +83,7 @@ export const UserConsents = () => {
               hasIcon={true}
               icon={CubesIcon}
               message={t("users:noConsents")}
-              instructions={
-                <div>
-                  {" "}
-                  {t("users:noConsentsText")}{" "}
-                  <Button
-                    variant="link"
-                    className="kc-settings-link-empty-state"
-                  >
-                    {t("common:settings") + "."}
-                  </Button>
-                </div>
-              }
+              instructions={t("users:noConsentsText")}
               onPrimaryAction={() => {}}
             />
           }
