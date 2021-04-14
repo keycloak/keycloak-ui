81d868e2cbcc89239a17ff80ad002fc2e74af9f8
jenny-s51 <jshandel@redhat.com>
2021-04-14 16:13:43 -0400

    remove log stmt

--- src/clients/add/CapabilityConfig.tsx
+++ src/clients/add/CapabilityConfig.tsx
@@ -28,8 +28,6 @@ export const CapabilityConfig = ({
   const clientAuthentication = watch("publicClient");
   const clientAuthorization = watch("authorizationServicesEnabled");
 
-  console.log(clientAuthentication);
-
   return (
     <FormAccess isHorizontal role="manage-clients" unWrap={unWrap}>
       <>
