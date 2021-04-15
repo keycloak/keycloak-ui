cb44734b503dc08e9dc98038a7977e713a3c79ee
Eugenia <32821331+jenny-s51@users.noreply.github.com>
2021-04-15 11:35:38 -0400

    fix lint
    Co-authored-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- src/realm-settings/RealmSettingsSection.tsx
+++ src/realm-settings/RealmSettingsSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Controller, useForm } from "react-hook-form";
