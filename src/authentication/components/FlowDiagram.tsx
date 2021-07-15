4401d07906b8a5ec6e3da906514876448b06a680
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-07-15 09:56:31 +0200

    use dagre to layout automatically

--- src/authentication/components/FlowDiagram.tsx
+++ src/authentication/components/FlowDiagram.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactFlow, { Elements, Position } from "react-flow-renderer";
+import ReactFlow, { Elements, Position, isNode } from "react-flow-renderer";
+import dagre from "dagre";
 
 import type { ExecutionList, ExpandableExecution } from "../execution-model";
 
@@ -28,7 +29,7 @@ const convert = (expandableList: ExpandableExecution[]) => {
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
       data: { label: ex.displayName! },
-      position: { x: 200 * (ex.level! + 1), y: 25 + 100 * ex.index! },
+      position: { x: 0, y: 0 },
     });
     if (ex.executionList) {
       const subFlow = ex.executionList;
@@ -44,13 +45,54 @@ const convert = (expandableList: ExpandableExecution[]) => {
 export const FlowDiagram = ({
   executionList: { expandableList },
 }: FlowDiagramProps) => {
+  const dagreGraph = new dagre.graphlib.Graph();
+  dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+  const nodeWidth = 130;
+  const nodeHeight = 28;
+
+  const getLayoutedElements = (elements: Elements, direction = "TB") => {
+    const isHorizontal = direction === "LR";
+    dagreGraph.setGraph({ rankdir: direction });
+
+    elements.forEach((element) => {
+      if (isNode(element)) {
+        dagreGraph.setNode(element.id, {
+          width: nodeWidth,
+          height: nodeHeight,
+        });
+      } else {
+        dagreGraph.setEdge(element.source, element.target);
+      }
+    });
+
+    dagre.layout(dagreGraph);
+
+    return elements.map((element) => {
+      if (isNode(element)) {
+        const nodeWithPosition = dagreGraph.node(element.id);
+        element.targetPosition = isHorizontal ? Position.Left : Position.Top;
+        element.sourcePosition = isHorizontal
+          ? Position.Right
+          : Position.Bottom;
+
+        element.position = {
+          x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
+          y: nodeWithPosition.y - nodeHeight / 2,
+        };
+      }
+
+      return element;
+    });
+  };
+
   let elements = convert(expandableList);
   elements.unshift({
     id: "start",
     sourcePosition: Position.Right,
     type: "input",
     data: { label: "Start" },
-    position: { x: 20, y: 25 },
+    position: { x: 0, y: 0 },
     className: "keycloak__authentication__input_node",
   });
   elements.push({
@@ -58,11 +100,11 @@ export const FlowDiagram = ({
     targetPosition: Position.Left,
     type: "output",
     data: { label: "End" },
-    position: { x: 800, y: 25 },
+    position: { x: 0, y: 0 },
     className: "keycloak__authentication__output_node",
   });
 
   elements = elements.concat(createEdge("start", expandableList));
 
-  return <ReactFlow elements={elements} />;
+  return <ReactFlow elements={getLayoutedElements(elements, "LR")} />;
 };
