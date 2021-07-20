ce0a98bdcb6fd2ba8bce577239b6dfd40d7ef643
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-07-20 16:42:09 +0200

    also do move so we can merge

--- src/authentication/messages.ts
+++ src/authentication/messages.ts
@@ -21,5 +21,28 @@ export default {
     copyOf: "Copy of {{name}}",
     copyFlowSuccess: "Flow successfully duplicated",
     copyFlowError: "Could not duplicate flow: {{error}}",
-  },
-};
+    "createFlow": "Create flow",
+    "flowType": "Flow type",
+    "flow-type": {
+      "basic-flow": "Basic flow",
+      "client-flow": "Client flow"
+    },
+    "flowCreatedSuccess": "Flow created",
+    "flowCreateError": "Could not create flow: {{error}}",
+    "flowDetails": "Flow details",
+    "emptyExecution": "No steps",
+    "emptyExecutionInstructions": "You can start defining this flow by adding a sub-flow or an execution",
+    "addExecutionTitle": "Add an execution",
+    "addExecution": "Add execution",
+    "addSubFlowTitle": "Add a sub-flow",
+    "addSubFlow": "Add sub-flow",
+    "steps": "Steps",
+    "requirement": "Requirement",
+    "requirements": {
+      "REQUIRED": "Required",
+      "ALTERNATIVE": "Alternative",
+      "DISABLED": "Disabled",
+      "CONDITIONAL": "Conditional"
+    }
+  }
+}
