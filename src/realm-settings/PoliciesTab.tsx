15aaaa6025d92a5c95d3915eb9fbd68eb60cf420
jenny-s51 <jshandel@redhat.com>
2021-10-08 15:47:57 -0400

    replace hardcoded URL with path

--- src/realm-settings/PoliciesTab.tsx
+++ src/realm-settings/PoliciesTab.tsx
@@ -27,6 +27,7 @@ import { useAlerts } from "../components/alert/Alerts";
 import "./RealmSettingsSection.css";
 import { useRealm } from "../context/realm-context/RealmContext";
 import { toNewClientPolicy } from "./routes/NewClientPolicy";
+import { toClientPolicies } from "./routes/ClientPolicies";
 export const PoliciesTab = () => {
   const { t } = useTranslation("realm-settings");
   const adminClient = useAdminClient();
@@ -100,7 +101,7 @@ export const PoliciesTab = () => {
         });
         addAlert(t("deleteClientPolicySuccess"), AlertVariant.success);
         refresh();
-        history.push(`/${realm}/realm-settings/clientPolicies`);
+        history.push(toClientPolicies({ realm }));
       } catch (error) {
         addError(t("deleteClientPolicyError"), error);
       }
