99ff8ccb22fb2f8aaf468381e8f577c19ab6bb5f
Agnieszka Gancarczyk <agancarc@redhat.com>
2021-10-27 11:49:04 +0100

    fixed a breadcrum for edit profile

--- src/realm-settings/routes/ClientProfile.ts
+++ src/realm-settings/routes/ClientProfile.ts
@@ -2,6 +2,7 @@ import type { LocationDescriptorObject } from "history";
 import { generatePath } from "react-router-dom";
 import type { RouteDef } from "../../route-config";
 import { ClientProfileForm } from "../ClientProfileForm";
+import { EditProfileCrumb } from "../RealmSettingsSection";
 
 export type ClientProfileParams = {
   realm: string;
@@ -11,7 +12,7 @@ export type ClientProfileParams = {
 export const ClientProfileRoute: RouteDef = {
   path: "/:realm/realm-settings/clientPolicies/:profileName",
   component: ClientProfileForm,
-  breadcrumb: (t) => t("realm-settings:clientProfile"),
+  breadcrumb: () => EditProfileCrumb,
   access: ["view-realm", "view-users"],
 };
 
