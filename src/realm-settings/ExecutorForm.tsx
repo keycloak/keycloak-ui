7986220392a744134755f318821702baa975651f
Agnieszka Gancarczyk <agancarc@redhat.com>
2021-11-04 17:44:23 +0000

    improved edit executor

--- src/realm-settings/ExecutorForm.tsx
+++ src/realm-settings/ExecutorForm.tsx
@@ -158,23 +158,13 @@ export default function ExecutorForm() {
 
   const newProfileExecutors = profileExecutorType?.properties.map(
     (property) => {
-      return {
-        helpText: property.helpText!,
-        label: property.label!,
-        name: property.name!,
-        defaultValue: "",
-        type: property.type!,
-      };
-    }
-  );
+      const globalDefaultValues = editMode ? property.defaultValue : "";
 
-  const globalProfileExecutors = profileExecutorType?.properties.map(
-    (property) => {
       return {
         helpText: property.helpText!,
         label: property.label!,
         name: property.name!,
-        defaultValue: property.defaultValue,
+        defaultValue: globalDefaultValues,
         type: property.type!,
       };
     }
@@ -271,28 +261,7 @@ export default function ExecutorForm() {
               }
             })}
             {editMode &&
-              !globalProfile &&
               newProfileExecutors?.map((option) => {
-                const componentType = option.type!;
-                if (isValidComponentType(componentType)) {
-                  const Component = COMPONENTS[componentType];
-                  return (
-                    <Component
-                      key={option.name}
-                      {...option}
-                      name={option.name}
-                      label={option.label}
-                    />
-                  );
-                } else {
-                  console.warn(
-                    `There is no editor registered for ${componentType}`
-                  );
-                }
-              })}
-            {editMode &&
-              globalProfile &&
-              globalProfileExecutors?.map((option) => {
                 const componentType = option.type!;
                 if (isValidComponentType(componentType)) {
                   const Component = COMPONENTS[componentType];
