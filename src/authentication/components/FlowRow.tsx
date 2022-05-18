60e7c34f64c207ea6599f7468de022f05157a692
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-05-18 08:45:14 +0200

    small fix

--- src/authentication/components/FlowRow.tsx
+++ src/authentication/components/FlowRow.tsx
@@ -15,7 +15,6 @@ import {
 } from "@patternfly/react-core";
 import { TrashIcon } from "@patternfly/react-icons";
 
-import type AuthenticationExecutionInfoRepresentation from "@keycloak/keycloak-admin-client/lib/defs/authenticationExecutionInfoRepresentation";
 import type { AuthenticationProviderRepresentation } from "@keycloak/keycloak-admin-client/lib/defs/authenticatorConfigRepresentation";
 import type { ExpandableExecution } from "../execution-model";
 import type { Flow } from "./modals/AddSubFlowModal";
@@ -30,7 +29,7 @@ type FlowRowProps = {
   builtIn: boolean;
   execution: ExpandableExecution;
   onRowClick: (execution: ExpandableExecution) => void;
-  onRowChange: (execution: AuthenticationExecutionInfoRepresentation) => void;
+  onRowChange: (execution: ExpandableExecution) => void;
   onAddExecution: (
     execution: ExpandableExecution,
     type: AuthenticationProviderRepresentation
