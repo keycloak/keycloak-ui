eb449f341146078c2087576a8ea3e1fd584a56b2
Jon Koops <jonkoops@gmail.com>
2022-06-14 16:11:45 +0200

    Add plugin to check TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true })],
 });
