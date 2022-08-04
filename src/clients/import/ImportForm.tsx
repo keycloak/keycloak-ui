d64d6b6d885779792f996b23642f9800acbdaa5d
Jon Koops <jonkoops@gmail.com>
2022-08-04 12:12:05 +0200

    Inverse response ok check

--- src/clients/import/ImportForm.tsx
+++ src/clients/import/ImportForm.tsx
@@ -73,7 +73,7 @@ export default function ImportForm() {
       }
     );
 
-    if (response.ok) {
+    if (!response.ok) {
       throw new Error(
         `Server responded with invalid status: ${response.statusText}`
       );
