9bb910ad981f8b0f96ccc300f685e4f19bc94bd4
Jon Koops <jonkoops@gmail.com>
2022-08-09 11:56:36 +0200

    Show error message if application bootstrapping fails

---
+++ src/AppError.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from "react";
+
+export const AppError: FunctionComponent = () => {
+  return <h1>Something went wrong</h1>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,41 @@ import "@patternfly/react-core/dist/styles/base.css";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import { App } from "./App";
+import { AdminClientProps, App } from "./App";
+import { AppError } from "./AppError";
 import { initAdminClient } from "./context/auth/AdminClient";
 import { initI18n } from "./i18n";
 
 import "./index.css";
 
+const root = document.getElementById("app");
+
 async function initialize() {
-  const { keycloak, adminClient } = await initAdminClient();
+  try {
+    const { keycloak, adminClient } = await initAdminClient();
 
-  await initI18n(adminClient);
+    await initI18n(adminClient);
+    renderApp({ keycloak, adminClient });
+  } catch (err) {
+    renderError();
+  }
+}
 
+function renderApp(props: AdminClientProps) {
   ReactDOM.render(
     <StrictMode>
-      <App keycloak={keycloak} adminClient={adminClient} />
+      <App {...props} />
     </StrictMode>,
-    document.getElementById("app")
+    root
+  );
+}
+
+function renderError() {
+  ReactDOM.render(
+    <StrictMode>
+      <AppError />
+    </StrictMode>,
+    root
   );
 }
 
