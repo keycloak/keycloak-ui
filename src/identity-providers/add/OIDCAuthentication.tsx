fd5c264908bece4645c04433b61aed211226e9ce
Ivan Khomyn <ikhomyn@redhat.com>
2022-08-09 13:45:51 +0200

    added translation for OIDC choices for Client authentication

--- src/identity-providers/add/OIDCAuthentication.tsx
+++ src/identity-providers/add/OIDCAuthentication.tsx
@@ -11,19 +11,18 @@ import {
 import { ClientIdSecret } from "../component/ClientIdSecret";
 import { HelpItem } from "../../components/help-enabler/HelpItem";
 
-const clientAuthenticationTypes = [
-  "client_secret_post",
-  "client_secret_basic",
-  "client_secret_jwt",
-  "private_key_jwt",
-];
-
 export const OIDCAuthentication = ({ create = true }: { create?: boolean }) => {
   const { t } = useTranslation("identity-providers");
 
   const { control } = useFormContext();
   const [openClientAuth, setOpenClientAuth] = useState(false);
 
+  const clientAuthentications = [
+    "clientAuth_post",
+    "clientAuth_basic",
+    "clientAuth_secret_jwt",
+    "clientAuth_privatekey_jwt",
+  ];
   const clientAuthMethod = useWatch({
     control: control,
     name: "config.clientAuthMethod",
@@ -42,24 +41,24 @@ export const OIDCAuthentication = ({ create = true }: { create?: boolean }) => {
         fieldId="clientAuthentication"
       >
         <Controller
-          name="config.clientAuthMethod"
-          defaultValue={clientAuthenticationTypes[0]}
+          name="config.clientAuthentications"
+          defaultValue={clientAuthentications[0]}
           control={control}
           render={({ onChange, value }) => (
             <Select
-              toggleId="clientAuthMethod"
+              toggleId="clientAuthentication"
               required
               onToggle={() => setOpenClientAuth(!openClientAuth)}
               onSelect={(_, value) => {
                 onChange(value as string);
                 setOpenClientAuth(false);
               }}
-              selections={value}
+              selections={t(`${value}`)}
               variant={SelectVariant.single}
-              aria-label={t("prompt")}
+              aria-label={t("clientAuthentication")}
               isOpen={openClientAuth}
             >
-              {clientAuthenticationTypes.map((option) => (
+              {clientAuthentications.map((option) => (
                 <SelectOption
                   selected={option === value}
                   key={option}
