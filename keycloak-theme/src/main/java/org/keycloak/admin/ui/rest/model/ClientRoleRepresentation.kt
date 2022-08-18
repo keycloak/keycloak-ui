919006c9b44f0221bfb558f86fe57cc19940a41a
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-08-18 15:16:58 +0200

    use new endpoints

--- keycloak-theme/src/main/java/org/keycloak/admin/ui/rest/model/ClientRoleRepresentation.kt
+++ keycloak-theme/src/main/java/org/keycloak/admin/ui/rest/model/ClientRoleRepresentation.kt
@@ -1,4 +1,4 @@
 package org.keycloak.admin.ui.rest.model
 
-data class ClientRoleRepresentation(var client: String?, var role: String, var description: String?) {
+data class ClientRoleRepresentation(var id: String, var role: String, var description: String?, var client: String?) {
 }
