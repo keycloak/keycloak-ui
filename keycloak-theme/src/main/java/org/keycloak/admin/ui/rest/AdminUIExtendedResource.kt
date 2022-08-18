e90cebbeae42bea34e3db66c19ce817b9d2e280f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-08-18 15:14:16 +0200

    added search and roles endpoint

--- keycloak-theme/src/main/java/org/keycloak/admin/ui/rest/AdminUIExtendedResource.kt
+++ keycloak-theme/src/main/java/org/keycloak/admin/ui/rest/AdminUIExtendedResource.kt
@@ -1,6 +1,7 @@
 package org.keycloak.admin.ui.rest
 
 import org.eclipse.microprofile.openapi.annotations.Operation
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
 import org.eclipse.microprofile.openapi.annotations.media.Content
 import org.eclipse.microprofile.openapi.annotations.media.Schema
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
@@ -9,7 +10,6 @@ import org.keycloak.admin.ui.rest.model.RoleMapper
 import org.keycloak.models.KeycloakSession
 import org.keycloak.models.RealmModel
 import org.keycloak.models.RoleModel
-import org.keycloak.representations.idm.RoleRepresentation
 import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator
 import org.mapstruct.factory.Mappers
 import java.util.*
@@ -28,7 +28,7 @@ open class AdminUIExtendedResource(
     var session: KeycloakSession? = null
 
     @GET
-    @Path("/client-scope/{id}")
+    @Path("/clientScopes/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(
@@ -40,23 +40,25 @@ open class AdminUIExtendedResource(
         description = "",
         content = [Content(
             schema = Schema(
-                implementation = RoleRepresentation::class
+                type = SchemaType.ARRAY,
+                implementation = ClientRoleRepresentation::class
             )
         )]
     )
     fun listCompositeClientScopeRoleMappings(
         @PathParam("id") id: String,
         @QueryParam("first") @DefaultValue("0") first: Long,
-        @QueryParam("max") @DefaultValue("10") max: Long
+        @QueryParam("max") @DefaultValue("10") max: Long,
+        @QueryParam("search") @DefaultValue("") search: String
     ): List<ClientRoleRepresentation> {
         val scopeContainer = realm.getClientScopeById(id) ?: throw NotFoundException("Could not find client scope")
         auth.clients().requireView(scopeContainer)
 
-        return availableMapping(Predicate<RoleModel?> { r -> scopeContainer.hasDirectScope(r) }.negate(), first, max)
+        return availableMapping(Predicate<RoleModel?> { r -> scopeContainer.hasDirectScope(r) }.negate(), first, max, search)
     }
 
     @GET
-    @Path("/client/{id}")
+    @Path("/clients/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(
@@ -68,23 +70,25 @@ open class AdminUIExtendedResource(
         description = "",
         content = [Content(
             schema = Schema(
-                implementation = RoleRepresentation::class
+                type = SchemaType.ARRAY,
+                implementation = ClientRoleRepresentation::class
             )
         )]
     )
     fun listCompositeClientRoleMappings(
         @PathParam("id") id: String,
         @QueryParam("first") @DefaultValue("0") first: Long,
-        @QueryParam("max") @DefaultValue("10") max: Long
+        @QueryParam("max") @DefaultValue("10") max: Long,
+        @QueryParam("search") @DefaultValue("") search: String
     ): List<ClientRoleRepresentation> {
         val scopeContainer = realm.getClientById(id) ?: throw NotFoundException("Could not find client")
         auth.clients().requireView(scopeContainer)
 
-        return availableMapping(Predicate<RoleModel?> { r -> scopeContainer.hasDirectScope(r) }.negate(), first, max)
+        return availableMapping(Predicate<RoleModel?> { r -> scopeContainer.hasDirectScope(r) }.negate(), first, max, search)
     }
 
     @GET
-    @Path("/group/{id}")
+    @Path("/groups/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(
@@ -96,24 +100,26 @@ open class AdminUIExtendedResource(
         description = "",
         content = [Content(
             schema = Schema(
-                implementation = RoleRepresentation::class
+                type = SchemaType.ARRAY,
+                implementation = ClientRoleRepresentation::class
             )
         )]
     )
     fun listCompositeGroupRoleMappings(
         @PathParam("id") id: String,
         @QueryParam("first") @DefaultValue("0") first: Long,
-        @QueryParam("max") @DefaultValue("10") max: Long
+        @QueryParam("max") @DefaultValue("10") max: Long,
+        @QueryParam("search") @DefaultValue("") search: String
     ): List<ClientRoleRepresentation> {
         val scopeContainer = realm.getGroupById(id) ?: throw NotFoundException("Could not find group")
         auth.groups().requireView(scopeContainer)
 
-        return availableMapping(Predicate<RoleModel?> { r -> scopeContainer.hasDirectRole(r) }.negate(), first, max)
+        return availableMapping(Predicate<RoleModel?> { r -> scopeContainer.hasDirectRole(r) }.negate(), first, max, search)
 
     }
 
     @GET
-    @Path("/user/{id}")
+    @Path("/users/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(
@@ -125,27 +131,63 @@ open class AdminUIExtendedResource(
         description = "",
         content = [Content(
             schema = Schema(
-                implementation = RoleRepresentation::class
+                type = SchemaType.ARRAY,
+                implementation = ClientRoleRepresentation::class
             )
         )]
     )
     fun listCompositeUserRoleMappings(
         @PathParam("id") id: String,
         @QueryParam("first") @DefaultValue("0") first: Long,
-        @QueryParam("max") @DefaultValue("10") max: Long
+        @QueryParam("max") @DefaultValue("10") max: Long,
+        @QueryParam("search") @DefaultValue("") search: String
     ): List<ClientRoleRepresentation> {
         val user = session?.users()?.getUserById(realm, id)
             ?: if (auth.users().canQuery()) throw NotFoundException("User not found") else throw ForbiddenException()
         auth.users().requireView(user);
 
-        return availableMapping(Predicate<RoleModel?> { r -> user.hasDirectRole(r) }.negate(), first, max)
+        return availableMapping(Predicate<RoleModel?> { r -> user.hasDirectRole(r) }.negate(), first, max, search)
 
+    }
+
+    @GET
+    @Path("/roles/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary = "List all composite client roles",
+        description = "This endpoint returns all the client role"
+    )
+    @APIResponse(
+        responseCode = "200",
+        description = "",
+        content = [Content(
+            schema = Schema(
+                type = SchemaType.ARRAY,
+                implementation = ClientRoleRepresentation::class
+            )
+        )]
+    )
+    fun listCompositeRoleMappings(
+        @QueryParam("first") @DefaultValue("0") first: Long,
+        @QueryParam("max") @DefaultValue("10") max: Long,
+        @QueryParam("search") @DefaultValue("") search: String
+    ): List<ClientRoleRepresentation> {
+        val clients = realm.clientsStream
+        val mapper = Mappers.getMapper(RoleMapper::class.java)
+        return clients
+            .flatMap { c -> c.rolesStream }
+            .map { r -> mapper.convertToRepresentation(r, realm.clientsStream) }
+            .skip(first)
+            .limit(max)
+            .collect(Collectors.toList()) ?: Collections.emptyList()
     }
 
     private fun availableMapping(
         predicate: Predicate<RoleModel?>,
         first: Long,
-        max: Long
+        max: Long,
+        search: String
     ): List<ClientRoleRepresentation> {
         val clients = realm.clientsStream
         val mapper = Mappers.getMapper(RoleMapper::class.java)
@@ -155,6 +197,7 @@ open class AdminUIExtendedResource(
             .filter(auth.roles()::canMapClientScope)
 
             .map { r -> mapper.convertToRepresentation(r, realm.clientsStream) }
+            .filter { r -> r.client?.indexOf(search) != -1 || r.role.indexOf(search) != -1 }
             .skip(first)
             .limit(max)
             .collect(Collectors.toList()) ?: Collections.emptyList()
