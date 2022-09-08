45f147f23d8e1079e1594c2a8eebb4531e7abb9b
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-09-08 11:31:07 +0200

    trying a build with different settings

--- apps/admin-ui/vite.config.ts
+++ apps/admin-ui/vite.config.ts
@@ -10,18 +10,7 @@ export default defineConfig({
   },
   resolve: {
     // Resolve the 'module' entrypoint at all times (not the default due to Node.js compatibility issues).
-    mainFields: ["module"],
     dedupe: ["react", "react-dom"],
-  },
-  optimizeDeps: {
-    // Enable optimization of dependencies using esbuild (see https://vitejs.dev/guide/migration.html#using-esbuild-deps-optimization-at-build-time).
-    disabled: false,
-  },
-  build: {
-    commonjsOptions: {
-      // Ensure `@rollup/plugin-commonjs` is not loaded, using esbuild instead (see https://vitejs.dev/guide/migration.html#using-esbuild-deps-optimization-at-build-time).
-      include: [],
-    },
   },
   plugins: [react(), checker({ typescript: true })],
   test: {
