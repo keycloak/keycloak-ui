81b4c0ca0ee53fab24064f2c4e641f0b07e544d5
Ivan Khomyn <ikhomyn@redhat.com>
2022-10-06 14:18:04 +0200

    attempt to fix tests with network error & other test fixes

--- apps/admin-ui/cypress/e2e/clients_test.spec.ts
+++ apps/admin-ui/cypress/e2e/clients_test.spec.ts
@@ -968,6 +968,7 @@ describe("Clients test", () => {
         .checkTabExists(ClientsDetailsTab.Settings, true)
         .checkTabExists(ClientsDetailsTab.Roles, true)
         .checkTabExists(ClientsDetailsTab.Sessions, true)
+        .checkTabExists(ClientsDetailsTab.Authorization, true)
         .checkTabExists(ClientsDetailsTab.Permissions, true)
         .checkTabExists(ClientsDetailsTab.Advanced, true)
         .checkNumberOfTabsIsEqual(5);
